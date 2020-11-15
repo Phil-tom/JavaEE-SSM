5662dd057be4d8ebd787cb5ffd5ee93f35fc7782
Phil-tom <zzz1565789907@163.com>
2020-11-15 20:00:04 +0800

    MVC中的M和C未分离

--- SpringMVC_Controller/src/controller/UserController.java
+++ SpringMVC_Controller/src/controller/UserController.java
@@ -19,16 +19,15 @@ import service.UserService;
 public class UserController {
     //得到一个用来记录日志的对象，这样打印信息的时候能够标记打印的是那个类的信息
     private static final Log logger = LogFactory.getLog(UserController.class);
-    //将服务依赖注入到属性userService
-    @Autowired
-    public UserService userService;
 
     /**
      * 处理登录
+     * 使用UserForm对象（实体Bean）user接收注册界面提交的请求参数
      */
     @RequestMapping("/login")
     public String login(UserForm user, HttpSession session, Model model) {
-        if (userService.login(user)) {
+        if ("zhangsan".equals(user.getUname())
+                && "123456".equals(user.getUpass())) {
             session.setAttribute("u", user);
             logger.info("成功");
             return "main";//登录成功，跳转到main.jsp
